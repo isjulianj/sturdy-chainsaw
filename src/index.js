18a550ab64313cff4a11c79d4302e3c34fdecff7
Julian Jackson <julian@kramphub.com>
2022-04-09 11:26:30 +0200

    requirement 2 - return error for invalid Ids

--- src/index.js
+++ src/index.js
@@ -24,22 +24,35 @@ function stockCheck(id) {
  * END DO NOT MODIFY
  * DISCLAIMER I modified the param type within the doc
  **/
+//  It specifies not to edit, normally I would correct the doctype for stockCheck, param is wrong.
+
+const ID_REGEX = /^[0-9-]{19}$/
+
+const ERROR_ENUM = {
+  invalid: "invalid-format",
+  outOfStock: "outOFStock"
+}
 
 /**
-* Identifies out of stock items
-* @param {...args: string | string[]} ...args  a list of product Ids to verify whether they are in stock
-* @returns {Promise} resolve/reject to out of stock products or error code
-**/
+ * Identifies out of stock items
+ * @param {...args: string | string[]} ...args  a list of product Ids to verify whether they are in stock
+ * @returns {Promise<any> | {error: {code: string, products: string[]}}} resolve/reject to out of stock products or error code
+ **/
 function outOfStockChecker() {
-  // simplify parameters into single array
+  // simplify parameters into single array and remove duplicates
   let idsToCheck;
-  if (arguments.length > 1 ) {
+  if (arguments.length > 1) {
     idsToCheck = removeDuplicateIds([...arguments])
   } else {
     idsToCheck = removeDuplicateIds(arguments[0])
   }
+  console.log('idsToCheck', idsToCheck)
 
-    console.log('idsToCheck', idsToCheck)
+  // Check for any of the ids are invalid
+  const invalidIds = getInvalidIds(idsToCheck)
+  if (invalidIds.length > 0) {
+    return {"error": {"code": ERROR_ENUM.invalid, "products": [...invalidIds]}}
+  }
 
 }
 
@@ -53,4 +66,14 @@ function removeDuplicateIds(idArray) {
     return Array.from(new Set(idArray))
 }
 
-module.exports = outOfStockChecker
+/**
+ * Checks for invalid ids withinan array
+ * all Ids should match the format "XXXX-XXXX-XXXX-XXXX" where X is any integer value.
+ * @param { string[] } idArray
+ */
+function getInvalidIds(idArray) {
+  return idArray.filter(id => ID_REGEX.exec(id) === null);
+}
+
+
+module.exports = outOfStockChecker;
